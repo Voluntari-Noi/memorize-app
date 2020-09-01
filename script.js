79d1286f19e822240e6b069689fc1689d4b190ee
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-09-01 21:45:27 +0300

    Choose a random text, from a list of texts.

--- script.js
+++ script.js
@@ -16,8 +16,43 @@ function shuffle(array) {
 }
 
 $.fn.BlankWordsTest = function() {
-  var texts = [];
-  var text_definition = "[Cuvântul] Tău este o [candelă] pentru [picioarele] mele și o [lumină] pe [cărarea] mea.";
+  var all_texts = [
+    "[Cuvântul] Tău este o [candelă] pentru [picioarele] mele și o [lumină] pe [cărarea] mea. (Psalmii 119:105)",
+    "Să [ascultăm], dar, [încheierea] tuturor [învățăturilor]: Teme-te de [Dumnezeu] și păzește [poruncile] Lui. Aceasta este [datoria] oricărui om. (Eclesiastul 12:13)"
+  ];
+  var number_of_texts = all_texts.length;
+  var text_definition = all_texts[Math.floor(Math.random() * (number_of_texts))];
+
+/*
+Proverbe 21
+2. Omul socotește că toate căile lui sunt fără prihană, dar Cel ce cercetează inimile este Domnul.
+Psalmi 34
+10. Puii de leu duc lipsă și li-e foame, dar cei ce caută pe Domnul nu duc lipsă de niciun bine.
+Psalmi 138
+8. Domnul va sfârși ce a început pentru mine. Doamne, bunătatea Ta ține în veci: nu părăsi lucrările mâinilor Tale.
+Iov 22
+21. Împrietenește-te dar cu Dumnezeu și vei avea pace; te vei bucura astfel iarăși de fericire.
+Romani 8
+5. În adevăr, cei ce trăiesc după îndemnurile firii pământești umblă după lucrurile firii pământești; pe când cei ce trăiesc după îndemnurile Duhului umblă după lucrurile Duhului.
+Romani 8
+31. Deci ce vom zice noi în fața tuturor acestor lucruri? Dacă Dumnezeu este pentru noi, cine va fi împotriva noastră?
+Romani 8
+38. Căci sunt bine încredințat că nici moartea, nici viața, nici îngerii, nici stăpânirile, nici puterile, nici lucrurile de acum, nici cele viitoare,
+39. nici înălțimea, nici adâncimea, nicio altă făptură, nu vor fi în stare să ne despartă de dragostea lui Dumnezeu care este în Isus Hristos, Domnul nostru.
+Evrei 13
+8. Isus Hristos este același ieri și azi și în veci!
+1 Petru 4
+17. Căci suntem în clipa când judecata stă să înceapă de la Casa lui Dumnezeu. Și, dacă începe cu noi, care va fi sfârșitul celor ce nu ascultă de Evanghelia lui Dumnezeu?
+Apocalipsa 14
+7. El zicea cu glas tare: „Temeți-vă de Dumnezeu și dați-I slavă, căci a venit ceasul judecății Lui; și închinați-vă Celui ce a făcut cerul și pământul, marea și izvoarele apelor!”
+8. Apoi a urmat un alt înger, al doilea, și a zis: „A căzut, a căzut Babilonul, cetatea cea mare, care a adăpat toate neamurile din vinul mâniei curviei ei!”
+9. Apoi a urmat un alt înger, al treilea, și a zis cu glas tare: „Dacă se închină cineva fiarei și icoanei ei și primește semnul ei pe frunte sau pe mână,
+10. va bea și el din vinul mâniei lui Dumnezeu, turnat neamestecat în paharul mâniei Lui; și va fi chinuit în foc și în pucioasă, înaintea sfinților îngeri și înaintea Mielului.
+11. Și fumul chinului lor se suie în sus în vecii vecilor. Și nici ziua, nici noaptea n-au odihnă cei ce se închină fiarei și icoanei ei și oricine primește semnul numelui ei!
+12. Aici este răbdarea sfinților, care păzesc poruncile lui Dumnezeu și credința lui Isus.”
+*/
+
+  // var text_definition = "[Cuvântul] Tău este o [candelă] pentru [picioarele] mele și o [lumină] pe [cărarea] mea.";
   var reg = /(?<=\[).+?(?=\])/g;
   var text_correct = text_definition.split("[").join("").split("]").join("");  // replace all [] with nothing
   var text_hidden = text_definition.match(reg);  // Extract the list of hidden words
