5760366c13ed2085a6d916f78e41343b21b0d476
Ileana B <70570143+ileanab@users.noreply.github.com>
2020-09-03 22:01:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ $.fn.BlankWordsTest = function() {
     "Domnul va [sfârși] ce a [început] pentru mine. Doamne, [bunătatea] Ta ține în [veci]: nu părăsi [lucrările] mâinilor Tale. (Psalmii 138:8)",
     "Împrietenește-te dar cu [Dumnezeu] și vei avea [pace]; te vei [bucura] astfel iarăși de [fericire]. (Iov 22:21)",
     "Deci ce vom [zice] noi în fața [tuturor] acestor [lucruri]? Dacă [Dumnezeu] este pentru [noi], cine va fi împotriva noastră? (Romani 8:31)",
+    "[Isus Hristos] este [același] [ieri] și [azi] și [în veci]! (Evrei 13:8)", 
     "În adevăr, cei ce [trăiesc] după îndemnurile firii [pământești] umblă după lucrurile [firii] pământești; pe când cei ce trăiesc după [îndemnurile] Duhului umblă după lucrurile [Duhului]. (Romani 8:5)"
   ];
   var number_of_texts = all_texts.length;
@@ -35,8 +36,6 @@ $.fn.BlankWordsTest = function() {
 Romani 8
 38. Căci sunt bine încredințat că nici moartea, nici viața, nici îngerii, nici stăpânirile, nici puterile, nici lucrurile de acum, nici cele viitoare,
 39. nici înălțimea, nici adâncimea, nicio altă făptură, nu vor fi în stare să ne despartă de dragostea lui Dumnezeu care este în Isus Hristos, Domnul nostru.
-Evrei 13
-8. Isus Hristos este același ieri și azi și în veci!
 1 Petru 4
 17. Căci suntem în clipa când judecata stă să înceapă de la Casa lui Dumnezeu. Și, dacă începe cu noi, care va fi sfârșitul celor ce nu ascultă de Evanghelia lui Dumnezeu?
 Apocalipsa 14
