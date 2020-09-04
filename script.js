6431168e627d305195723f99172d183b181a78c9
Ileana B <70570143+ileanab@users.noreply.github.com>
2020-09-04 14:47:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,21 @@ $.fn.BlankWordsTest = function() {
     "Apoi a urmat un alt [înger], al [treilea], și a zis cu glas tare: „Dacă se [închină] cineva [fiarei] și [icoanei] ei și primește [semnul] ei pe [frunte] sau pe [mână], va bea și el din [vinul] mâniei lui Dumnezeu, turnat neamestecat în [paharul] mâniei Lui; și va fi chinuit în [foc] și în [pucioasă], înaintea [sfinților îngeri] și înaintea [Mielului]. Și fumul chinului lor se suie în sus în vecii vecilor. Și nici [ziua], nici [noaptea] n-au odihnă cei ce se închină [fiarei] și [icoanei] ei și oricine primește [semnul] numelui ei! (Apocalipsa 14:9-11)",
     "Aici este [răbdarea] sfinților, care păzesc [poruncile] lui [Dumnezeu] și [credința] lui [Isus.]” (Apocalipsa 14:12)",
     "Căci sunt bine încredințat că nici [moartea], nici [viața], nici [îngerii], nici [stăpânirile], nici [puterile], nici lucrurile de [acum], nici cele [viitoare], nici [înălțimea], nici [adâncimea], nicio altă [făptură], nu vor fi în stare să ne [despartă] de dragostea lui Dumnezeu care este în Isus Hristos, Domnul [nostru]. (Romani 8:38,39) ",
-    "În adevăr, cei ce [trăiesc] după îndemnurile firii [pământești] umblă după lucrurile [firii] pământești; pe când cei ce trăiesc după [îndemnurile] Duhului umblă după lucrurile [Duhului]. (Romani 8:5)"
+    "În adevăr, cei ce [trăiesc] după îndemnurile firii [pământești] umblă după lucrurile [firii] pământești; pe când cei ce trăiesc după [îndemnurile] Duhului umblă după lucrurile [Duhului]. (Romani 8:5)",
+    "Căci suntem în [clipa] când [judecata] stă să înceapă de la [Casa] lui Dumnezeu. Și, dacă începe cu [noi], care va fi [sfârșitul] celor ce nu [ascultă] de [Evanghelia] lui Dumnezeu? (1 Petru 4:17)",
+    "Pot [totul] în [Hristos] care mă [întăreşte]. (Filipeni 4:13)",
+    "Cine are [poruncile] Mele şi le [păzeşte] acela [Mă iubeşte]; şi cine Mă iubeşte va fi iubit de Tatăl Meu. Eu îl voi iubi şi Mă voi arăta lui. (Ioan 14:21)",
+    "Atunci [toţi] cei ce [se încred] în Tine se vor [bucura], se vor [înveseli] totdeauna, căci Tu îi vei [ocroti]. Tu vei fi bucuria celor ce [iubesc] Numele Tău. (Psalmii 5:11)",
+    "Încredinţează-ţi [soarta] în [mâna] Domnului, şi El te va [sprijini]. El nu va [lăsa] niciodată să se [clatine] cel neprihănit. (Psalmii 55:22)",
+    "În [adevăr], [însuşirile] [nevăzute] ale Lui, [puterea] Lui [veşnică] şi [dumnezeirea] Lui se văd [lămurit], de la facerea lumii, când te uiţi cu băgare de seamă la ele în lucrurile făcute de El. Aşa că nu se pot dezvinovăţi. (Romani 1:20)",
+    "Acum, dar, rămân acestea trei: [credinţa], [nădejdea] şi [dragostea]; dar cea mai mare dintre ele este [dragostea]. (1 Corinteni 13:13)",
+    "Cei ce [cunosc] Numele Tău [se încred] în Tine, căci Tu nu [părăseşti] pe cei ce Te [caută], Doamne! (Psalmii 9:10)",
+    "Isus Hristos este [acelaşi] [ieri] şi [azi] şi [în veci]! (Evrei 13:8)",
+    "Şi Dumnezeu a zis: Iată că v-am dat orice [iarbă] care face [sămânţă] şi care este pe faţa întregului pământ şi orice [pom] care are în el rod cu [sămânţă]: aceasta să fie [hrana] voastră. (Genesa 1:29)",
+    "Căci [Domnul Dumnezeu] este un [soare] şi un [scut], [Domnul] dă [îndurare] şi [slavă], şi nu lipseşte de [niciun bine] pe cei ce duc o viaţă fără prihană. (Psalmii 84:11)",
+    "Nu te teme, căci Eu sunt [cu tine]; nu te uita cu îngrijorare, căci Eu sunt [Dumnezeul tău]; Eu te [întăresc], tot Eu [îţi vin în ajutor]. Eu [te sprijin] cu dreapta Mea biruitoare. (Isaia 41:10)",
+    "Căci mie nu mi-e ruşine de [Evanghelia lui Hristos]; fiindcă ea este [puterea lui Dumnezeu] pentru mântuirea fiecăruia care crede: [întâi] a [iudeului], apoi a [grecului]. (Romani 1:16)",
+    "El te va acoperi cu [penele] Lui şi te vei ascunde sub [aripile] Lui. Căci [scut] şi [pavăză] este [credincioşia] Lui! (Psalmii 91:4)"
   ];
   var number_of_texts = all_texts.length;
   var random_index = Math.floor(Math.random() * (number_of_texts));
