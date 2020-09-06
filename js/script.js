8de2fe90834fe0496560e4427c81a39ede1bb47f
Verdes Robert Gabriel <verdesg.robert@gmail.com>
2020-09-06 15:17:58 +0200

    Change Resource Caching

--- js/script.js
+++ js/script.js
@@ -137,3 +137,12 @@ $.fn.BlankWordsTest = function() {
     }
   });
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
---
+++ js/serviceWorker.js
@@ -0,0 +1,26 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/bootstrap-theme.min.css",
+  "/css/bootstrap.min.css",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
