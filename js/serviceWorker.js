dc9c363611be9deb78fdd498b9a113cfd87db20d
Verdes Robert Gabriel <verdesg.robert@gmail.com>
2020-09-06 15:34:06 +0200

    ServiceWorker relative Paths

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,11 +1,11 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
-  "/",
-  "index.html",
-  "css/bootstrap-theme.min.css",
-  "css/bootstrap.min.css",
-  "css/styles.css",
-  "js/script.js",
+  "..//",
+  "../index.html",
+  "../css/bootstrap-theme.min.css",
+  "../css/bootstrap.min.css",
+  "../css/styles.css",
+  "../js/script.js",
 ]
 
 self.addEventListener("install", installEvent => {
