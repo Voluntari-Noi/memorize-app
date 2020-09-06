cfcde07cfc1f34e0d79507916a0f07b3a8ca703b
Verdes Robert Gabriel <verdesg.robert@gmail.com>
2020-09-06 15:27:04 +0200

    Correct Paths

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,20 +1,11 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
   "/",
-  "/index.html",
-  "/css/bootstrap-theme.min.css",
-  "/css/bootstrap.min.css",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg",
+  "index.html",
+  "css/bootstrap-theme.min.css",
+  "css/bootstrap.min.css",
+  "css/styles.css",
+  "js/script.js",
 ]
 
 self.addEventListener("install", installEvent => {
